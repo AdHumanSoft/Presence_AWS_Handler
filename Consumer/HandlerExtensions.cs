82d777031d4887d12e5efea34bbae7a81a3b5a76
Oriol <kedalen@gmx.com>
2023-02-22 00:05:20 +0100

    Removed Publisher as it will be handled via the express api

--- Consumer/HandlerExtensions.cs
+++ Consumer/HandlerExtensions.cs
@@ -1,20 +1,30 @@
 using System.Reflection;
+using Amazon.SimpleEmail;
+using Amazon.SQS;
 using Consumer.Handlers;
 using Serilog;
 
 namespace Consumer;
 
 
-public static class HandlerExtensions {
+public static class HandlerExtensions
+{
 
-    public static IServiceCollection AddMessageHandlers(this IServiceCollection services) {
+
+    /// <summary>
+    /// Add all message handlers to the service collection
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddMessageHandlers(this IServiceCollection services)
+    {
         var handlers = Assembly.GetExecutingAssembly().DefinedTypes
             .Where(x => typeof(IMessageHandler).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract);
-            
+
         foreach (var handler in handlers)
-        {   
+        {
             var serviceDescriptor = new ServiceDescriptor(handler.AsType(), handler, ServiceLifetime.Scoped);
-            services.Add(serviceDescriptor);   
+            services.Add(serviceDescriptor);
         }
 
         Console.WriteLine("MessageHandlers added successfully {0}", handlers.Select(x => x.Name).Aggregate((a, b) => a + ", " + b));
@@ -22,6 +32,20 @@ public static class HandlerExtensions {
     }
 
 
+
+    /// <summary>
+    /// Add AWS services to the service collection
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="configuration"></param>
+    public static IServiceCollection AddAws(this IServiceCollection services)
+    {
+        Environment.SetEnvironmentVariable("AWS_REGION", "eu-west-2");
+        Environment.SetEnvironmentVariable("AWS_PROFILE", "sqs-test");
+        services.AddSingleton<IAmazonSimpleEmailService>(sp => new AmazonSimpleEmailServiceClient(Amazon.RegionEndpoint.EUWest2));
+        services.AddSingleton<IAmazonSQS>(sp => new AmazonSQSClient(Amazon.RegionEndpoint.EUWest2));
+        return services;
+    }
 
 
 }
--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -4,19 +4,13 @@ using Consumer;
 using Consumer.Utils;
 
 
-Environment.SetEnvironmentVariable("AWS_REGION", "eu-west-2");
-Environment.SetEnvironmentVariable("AWS_PROFILE", "sqs-test");
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAws();
 builder.Services.AddHostedService<SqsConsumerService>();
-builder.Services.AddSingleton<IAmazonSQS>(sp => new AmazonSQSClient(
-    Amazon.RegionEndpoint.EUWest2
-));
 builder.Services.AddSingleton<MessageDispatcher>();
-builder.Services.AddSingleton<IAmazonSimpleEmailService>(sp => new AmazonSimpleEmailServiceClient(   Amazon.RegionEndpoint.EUWest2
-));
 builder.Services.AddSingleton<Emailer>(x => new Emailer(x.GetRequiredService<IAmazonSimpleEmailService>()));
 
 builder.Services.AddMessageHandlers();
